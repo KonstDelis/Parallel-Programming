b8a3c19cea27679dcc1ba2e8fbaf635c96bcc0e4
KONSTANTINOS DELIS <csd4623@csd.uoc.gr>
2023-05-16 00:54:50 +0300

    converted from big decimal to double, and other fixes

--- hw3/JAVA_THREADS/BarnesHut/src/BHtree.java
+++ hw3/JAVA_THREADS/BarnesHut/src/BHtree.java
@@ -1,16 +1,21 @@
+import java.util.ArrayList;
+
 public class BHtree {
     Quad root;
 
     public BHtree(Boundary initialSpace){
-
+        root = new Quad(initialSpace);
     }
-    public BHtree(Boundary initialSpace, Planet planets[]){
-
+    public BHtree(Boundary initialSpace, Planet[] planets){
+        root = new Quad(initialSpace);
+        for(Planet planet : planets){
+            insertPlanet(planet);
+        }
     }
     public void insertPlanet(Planet planet){
-
+        root.insertPlanet(planet);
     }
     public void calculateForce(Planet p){
-
+        root.calculateForce(p);
     }
 }
